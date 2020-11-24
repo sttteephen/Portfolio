1a60b938a2e376df944883fdc39029996054ab2c
Stephen Messer <stephenmesser@Stephens-MacBook-Pro.local>
2020-11-24 20:09:01 +0000

    added icons

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function typeWriter() {
 }
 
 typeWriter();
+
+let root = document.documentElement;
+
+window.addEventListener('scroll', function() {
+
+	var windowHeight = window.innerHeight
+	var yOffset = window.pageYOffset
+
+	if (yOffset < (windowHeight-80)) {
+		root.style.setProperty('--navcolor', '#30475e');
+	}
+
+	if (yOffset > (windowHeight-80)) {
+		root.style.setProperty('--navcolor', '#f1935c');
+	} 
+
+	if (yOffset > (2*windowHeight)-80) {
+		root.style.setProperty('--navcolor', '#85a392');
+	}
+});
